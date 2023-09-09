0b79632608e36a43bf8062af99dab34255e7147c
U-AD\pogoreli <pogoreli@ECE419-6V259R3.ad.lafayette.edu>
2023-09-09 18:48:17 -0400

    Added picture capturing

--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -10,10 +10,15 @@ public class main : MonoBehaviour
     //private string folderPath = "C:/Users/pogoreli/Tag training/Assets/QR/textures/"; // The folder where your QR code textures are stored
     private string QRfolderPath;
     private string BackgroundfolderPath;
+    private string CapturedImagesPath;
+    private string CapturedCoordinatesPath;
 
     public GameObject tag;
     public GameObject camera;
     public GameObject light;
+
+    public GameObject LT;
+    public GameObject RB;
 
     private float tagX;
     private float tagY;
@@ -26,28 +31,53 @@ public class main : MonoBehaviour
     {
         QRfolderPath = Path.Combine(Application.dataPath, "QR/textures/");
         BackgroundfolderPath = Path.Combine(Application.dataPath, "Background/textures/");
+        CapturedImagesPath = Path.Combine(Application.dataPath, "captured/images/");
+        CapturedCoordinatesPath = Path.Combine(Application.dataPath, "captured/labels/");
+
+        if (!Directory.Exists(CapturedImagesPath))
+        {
+            Directory.CreateDirectory(CapturedImagesPath);
+        }
+
+        if (!Directory.Exists(CapturedCoordinatesPath))
+        {
+            Directory.CreateDirectory(CapturedCoordinatesPath);
+        }
     }
 
-    private int counter = 0;
     // Update is called once per frame
+    int goal = 1001;
+    int counter = 1;
     void Update()
     {
-        //if (counter % 10 == 0) 
+        //if (Input.GetKeyDown(KeyCode.Space))
         //{
-        //    
         //    ChangeBackgroundTextureRandomly();
         //    randomizeTagPosition();
         //    ChangeQRTextureRandomly();
+        //    randomizeCameraPosition();
+        //    randomizeLightPosition();
+        //    CaptureImage(1);
         //}
+        //yield return new WaitForSeconds(2.0f);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ChangeBackgroundTextureRandomly();
-            randomizeTagPosition();
-            ChangeQRTextureRandomly();
-            randomizeCameraPosition();
-            randomizeLightPosition();
+        Debug.Log("Image: " + (counter - 1) + " out of: " + (goal - 1));
+
+
+        ChangeBackgroundTextureRandomly();
+        randomizeTagPosition();
+        ChangeQRTextureRandomly();
+        randomizeCameraPosition();
+        randomizeLightPosition();
+        //yield return new WaitForSeconds(0.2f);
+        CaptureImage(counter++);
+        //yield return new WaitForSeconds(0.2f);
+        
+        if (counter >= goal) {
+            Application.Quit();
         }
+
+        
     }
 
     void ChangeQRTextureRandomly()
@@ -113,7 +143,7 @@ public class main : MonoBehaviour
     void randomizeCameraPosition()
     {
         float newX = tagX + Random.Range(-10f, 10f);
-        float newY = tagY + Random.Range(-10f, 10f);
+        float newY = tagY + Random.Range(-10f, 10f) + 10;
         float newZ = -255 + Random.Range(-50f, 200f);
 
         float newRoationX = Random.Range(0f, 10f);
@@ -149,6 +179,44 @@ public class main : MonoBehaviour
         Light lightComponent = light.GetComponent<Light>();
         lightComponent.intensity = newIntensity;
         lightComponent.color = new Color(newColorR, newColorG, newColorB, newAlpha);
+    }
 
+    void CaptureImage(int index)
+    {
+        Camera captureCamera = camera.GetComponent<Camera>();
+
+        int width = 1920;
+        int height = 1080;
+
+        // Create a RenderTexture
+        RenderTexture renderTexture = new RenderTexture(width, height, 24);
+        captureCamera.targetTexture = renderTexture;
+
+        // Capture the image
+        captureCamera.Render();
+
+        // Read the image into a Texture2D
+        RenderTexture.active = renderTexture;
+        Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
+        texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        texture.Apply();
+
+        // Convert to PNG and save
+        byte[] bytes = texture.EncodeToPNG();
+        File.WriteAllBytes(CapturedImagesPath + "image" + index.ToString("D4") + ".png", bytes);
+
+        // Get and save object coordinates
+        Vector2 lt = RectTransformUtility.WorldToScreenPoint(captureCamera, LT.transform.position);
+        Vector2 rb = RectTransformUtility.WorldToScreenPoint(captureCamera, RB.transform.position);
+
+        string coordinates = "1 " + (float)lt.x/(float)width + " " + (float)lt.y/(float)height + " " + (float)rb.x/(float)width + " " + (float)rb.y/(float)height;
+        File.WriteAllText(CapturedCoordinatesPath + "coordinates" + index.ToString("D4") + ".txt", coordinates);
+
+        captureCamera.targetTexture = null;
+
+        // Clean up
+        Destroy(texture);
+        RenderTexture.active = null;
+        Destroy(renderTexture);
     }
 }
